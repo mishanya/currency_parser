19e877101ecc33014afca83846a17d4051455fb3
mik <mik@azartnetwork.com>
2017-10-22 01:47:27 +0300

    Hot reload added

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
-document.getElementById('root').innerHTML = 'Привет, я готов.'
+import 'babel-polyfill'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './containers/App'
 
+ ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
+if (module.hot) {
+  module.hot.accept('./containers/App', () => {
+    const App = require('./containers/App').default;
+    ReactDOM.render(<App/>, document.getElementById('root'));
+  });
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,49 @@
-var path = require('path')
-var webpack = require('webpack')
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ScriptExtHtmlWebpackPlugin = require("script-ext-html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 module.exports = {
-  devtool: 'cheap-module-eval-source-map',
-  entry: [
-    'webpack-hot-middleware/client',
-    './src/index'
-  ],
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/static/'
+  entry:
+    [
+      'webpack-dev-server/client?http://0.0.0.0:3000', // WebpackDevServer host and port
+      'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
+      "./src/index.js"
+    ]
+  ,
+  devtool: "inline-source-map",
+  devServer: {
+    port: 3000
+  },
+  resolve: {
+    extensions: [".jsx", ".js"]
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'babel-loader',
+        exclude: "/node_modules/",
+        query: {
+          presets: ["react"]
+        }
+      }
+    ]
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoEmitOnErrorsPlugin()
-  ]
-}
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+      inject: "head"
+    }),
+    new ScriptExtHtmlWebpackPlugin({
+      defer: [],
+      defaultAttribute: 'defer'
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist")
+  }
+};
